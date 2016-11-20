1f1f0ab194ea18e3d590fec6d8a425b651bdd792
lencse <leventeloki@gmail.com>
2016-11-20 23:42:10 +0100

    Test solve() when n=3

--- test/test.ts
+++ test/test.ts
@@ -48,7 +48,7 @@ import { Position, pos } from "../src/Position";
         assert.equal(1, solved.val(pos(1, 1)));
     }
 
-        @test "solve n=2"() {
+    @test "solve n=2"() {
         let sudoku = Sudoku.create(2);
         sudoku.put(pos(1, 1), 1);
         sudoku.put(pos(1, 2), 3);
@@ -77,6 +77,63 @@ import { Position, pos } from "../src/Position";
         assert.equal(4, solved.val(pos(3, 3)));
         assert.equal(3, solved.val(pos(4, 3)));
         assert.equal(1, solved.val(pos(4, 4)));
+    }
+
+    @test "solve n=3"() {
+        let sudoku = Sudoku.create(3);
+        
+        sudoku.put(pos(1, 6), 1);
+
+        sudoku.put(pos(2, 1), 7);
+        sudoku.put(pos(2, 3), 6);
+        sudoku.put(pos(2, 4), 5);
+        sudoku.put(pos(2, 5), 8);
+        sudoku.put(pos(2, 9), 4);
+
+        sudoku.put(pos(3, 2), 2);
+        sudoku.put(pos(3, 6), 6);
+        sudoku.put(pos(3, 8), 3);
+        
+        sudoku.put(pos(4, 2), 3);
+        sudoku.put(pos(4, 5), 4);
+        sudoku.put(pos(4, 8), 5);
+        
+        sudoku.put(pos(5, 1), 4);
+        sudoku.put(pos(5, 3), 2);
+        sudoku.put(pos(5, 7), 7);
+        sudoku.put(pos(5, 9), 3);
+
+        sudoku.put(pos(6, 2), 1);
+        sudoku.put(pos(6, 5), 3);
+
+        sudoku.put(pos(7, 2), 8);
+        sudoku.put(pos(7, 4), 1);
+        sudoku.put(pos(7, 8), 9);
+
+        sudoku.put(pos(8, 1), 1);
+        sudoku.put(pos(8, 5), 2);
+        sudoku.put(pos(8, 7), 6);
+        sudoku.put(pos(8, 9), 5);
+
+        sudoku.put(pos(9, 4), 7);
+
+        let solved = sudoku.solve();
+
+        const solution = [ ,
+            [ , 3, 4, 8, 2, 9, 1, 5, 7, 6 ],
+            [ , 7, 9, 6, 5, 8, 3, 1, 2, 4 ],
+            [ , 5, 2, 1, 4, 7, 6, 8, 3, 9 ],
+            [ , 8, 3, 7, 6, 4, 2, 9, 5, 1 ],
+            [ , 4, 6, 2, 9, 1, 5, 7, 8, 3 ],
+            [ , 9, 1, 5, 8, 3, 7, 4, 6, 2 ],
+            [ , 6, 8, 3, 1, 5, 4, 2, 9, 7 ],
+            [ , 1, 7, 9, 3, 2, 8, 6, 4, 5 ],
+            [ , 2, 5, 4, 7, 6, 9, 3, 1, 8 ] ];
+        for (let row = 1; row <= 9; ++row) {
+            for (let col = 1; col <= 9; ++col) {
+                assert.equal(solution[row][col], solved.val(pos(row, col)));
+            }
+        }
     }
 
     @test "put-and-val n=1"() {
