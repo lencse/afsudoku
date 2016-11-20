a96b74ad58466eee91821402eee1d5e5ed5d5f98
lencse <leventeloki@gmail.com>
2016-11-20 21:33:23 +0100

    Store current position

--- src/Sudoku.ts
+++ src/Sudoku.ts
@@ -15,7 +15,10 @@ export abstract class Sudoku {
         for (let i = 0; i < Math.pow(n, 4); ++i) {
             this.cells.push(cells ? cells[i] : 0);
         }
+        this.setUp();
     }
+
+    protected abstract setUp();
 
     public val(position: Position): number {
         return this.cells[this.transformPositionToIndex(position)];
@@ -54,13 +57,31 @@ export class SudokuUnderSetup extends Sudoku {
         this.cells[this.transformPositionToIndex(position)] = value;
     }
 
+    protected setUp() {
+    }
+
 }
 
 export class SudokuUnderSolving extends Sudoku {
 
+    private modifyableCells: Array<number>;
+    private current: number;
+
+    protected setUp() {
+        this.modifyableCells = [];
+        this.current = 0;
+        this.cells.map((cell: number, idx: number) => {
+            if (cell == 0) {
+                this.modifyableCells.push(idx);
+            }
+        });
+    }
+
     public step(): SudokuUnderSolving {
         let next = new SudokuUnderSolving(this.n, this.cells);
-        next.cells[0] = 1;
+        next.modifyableCells = this.modifyableCells;
+        next.current = this.current;
+        next.cells[next.modifyableCells[next.current]] = 1;
         return next;
     }
 
